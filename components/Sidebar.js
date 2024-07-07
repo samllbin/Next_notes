45a6edb4fdb7dfc2f59df5e077a76b57b0a42890
samllbin <3057988675@qq.com>
2024-07-07 16:22:54 +0800

    侧边栏

---
+++ components/Sidebar.js
@@ -0,0 +1,36 @@
+import React, { Suspense } from "react";
+import Link from "next/link";
+
+import SidebarNoteList from "@/components/SidebarNoteList";
+import EditButton from "@/components/EditButton";
+import NoteListSkeleton from "@/components/NoteListSkeleton";
+
+export default async function Sidebar() {
+  return (
+    <>
+      <section className="col sidebar">
+        <Link href={"/"} className="link--unstyled">
+          <section className="sidebar-header">
+            <img
+              className="logo"
+              src="/logo.svg"
+              width="22px"
+              height="20px"
+              alt=""
+              role="presentation"
+            />
+            <strong>React Notes</strong>
+          </section>
+        </Link>
+        <section className="sidebar-menu" role="menubar">
+          <EditButton noteId={null}>New</EditButton>
+        </section>
+        <nav>
+          <Suspense fallback={<NoteListSkeleton />}>
+            <SidebarNoteList />
+          </Suspense>
+        </nav>
+      </section>
+    </>
+  );
+}
