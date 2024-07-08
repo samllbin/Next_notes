f9a27e53e0c70c17e6136a99f771698dfa82443c
samllbin <3057988675@qq.com>
2024-07-08 15:26:20 +0800

    note内容

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -3,7 +3,7 @@ import { getAllNotes } from "@/lib/redis";
 
 export default async function NoteList() {
   const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-  await sleep(10000);
+  await sleep(2000);
   const notes = await getAllNotes();
 
   const arr = Object.entries(notes);
